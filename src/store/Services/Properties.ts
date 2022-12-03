77df1efc88725d44d485fb9c96e2d9522bd75380
Ericka Castellanos <65922114+ErickaCastellanos@users.noreply.github.com>
2022-12-02 22:02:09 -0600

    NO

--- src/store/Services/Properties.ts
+++ src/store/Services/Properties.ts
@@ -1,3 +1,4 @@
 //Este archivo es el ncargado de obtener la información del backend
-//import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-//import { RootState } from "../store";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+
